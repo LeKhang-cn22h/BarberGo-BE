3e6b7bd31f7bc8b190c1eaeb3eb5f88e8dd4e9dc
LeKhang-cn22h <chube02082017@gmail.com>
2025-12-03 08:33:11 +0700

    feat: update supabase client and main application logic

--- app/main.py
+++ app/main.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI
 from app.api.acneAPI import router as acne_router
+from app.routers.user_router import router as user_router
 
 app = FastAPI(
-    title="Acne Detection API",
-    description="API phát hiện mụn sử dụng YOLOv8 và MediaPipe Face Mesh",
+    title="Acne Detection API and Supabase FastAPI",
+    description="API phát hiện mụn sử dụng YOLOv8 và MediaPipe Face Mesh và quản lý người dùng với Supabase",
     version="1.0.0"
 )
 
@@ -12,7 +13,7 @@ app.include_router(acne_router)
 @app.get("/")
 async def root():
     return {
-        "message": "Acne Detection API",
+        "message": "Acne Detection API, and Supabase FastAPI is running!",
         "version": "1.0.0",
         "docs": "/docs"
     }
