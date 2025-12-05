2d90a6561bceec1101732d0bd2cabcb9caf7b51d
hohuynhnhu <hhnhu2003@gmail.com>
2025-12-05 23:47:23 +0700

    register endpoint

--- app/main.py
+++ app/main.py
@@ -5,6 +5,7 @@ from app.routers.barbers_router import router as barbers_router
 from app.routers.service_router import router as service_router
 from app.routers.booking_router import router as booking_router
 from app.routers.ratings_router import router as ratings_router
+from app.routers.Hairstyle_router import router as Hairstyle_router
 app = FastAPI(
     title="Acne Detection API and Supabase FastAPI",
     description="API phát hiện mụn sử dụng YOLOv8 và MediaPipe Face Mesh và quản lý người dùng với Supabase",
@@ -17,6 +18,7 @@ app.include_router(barbers_router)
 app.include_router(service_router)
 app.include_router(booking_router)
 app.include_router(ratings_router)
+app.include_router(Hairstyle_router)
 
 @app.get("/")
 async def root():
